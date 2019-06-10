0a2a823b343feb5667be828a179a9cad0585532a
Eric Clark <eclark0013@gmail.com>
2019-06-10 20:30:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
-def reverse_each_word(string)
+def reverse_into_array(string)
   array=string.split(" ")
-  new_array=[]
   array.collect do |word|
     word.reverse
   end
+end
+
+def reverse_each_word(string)
+  reverse_into_array.join(" ")
 end
